7570e39b406fe167b60a74840330202a99a426a1
Guillaume Lessard <dhtnstff@gmail.com>
2017-05-08 14:25:17 -0600

    unused overrides

--- Xcode/streamTests/flatMapTests.swift
+++ Xcode/streamTests/flatMapTests.swift
@@ -11,18 +11,6 @@ import stream
 
 class flatMapTests: XCTestCase
 {
-  override func setUp()
-  {
-    super.setUp()
-    // Put setup code here. This method is called before the invocation of each test method in the class.
-  }
-
-  override func tearDown()
-  {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    super.tearDown()
-  }
-
   func testFlatMap1()
   {
     let s = EventStream<Int>()
