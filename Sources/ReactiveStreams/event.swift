ce10173812dbe1f1965e2f44aa70d363bdbc8968
Guillaume Lessard <dhtnstff@gmail.com>
2018-09-21 22:10:05 -0600

    replace Event with a typealias to Outcome (and an extension)

--- Sources/ReactiveStreams/event.swift
+++ Sources/ReactiveStreams/event.swift
@@ -6,110 +6,29 @@
 //  Copyright © 2015 Guillaume Lessard. All rights reserved.
 //
 
-public struct Event<Value>
+import Outcome
+
+public typealias Event = Outcome
+
+extension Event
 {
-  fileprivate let state: State<Value>
-
-  public init(value: Value)
+  public init(final: StreamCompleted)
   {
-    state = .value(value)
+    self.init(error: final)
   }
 
-  init(final: StreamCompleted)
-  {
-    state = .error(final)
-  }
-
-  public static var streamCompleted: Event {
+  public static var streamCompleted: Event<Value> {
     return Event(final: StreamCompleted.normally)
   }
 
-  public init(error: Error)
-  {
-    state = .error(error)
-  }
-
-  public func get() throws -> Value
-  {
-    switch state
-    {
-    case .value(let value): return value
-    case .error(let error): throw error
-    }
-  }
-
-  public var value: Value? {
-    if case .value(let value) = state { return value }
-    return nil
-  }
-
-  public var isValue: Bool {
-    if case .value = state { return true }
-    return false
-  }
-
   public var streamCompleted: StreamCompleted? {
-    if case .error(let final as StreamCompleted) = state
-    { return final }
-
-    return nil
+    return self.error as? StreamCompleted
   }
-
-  @available(*, unavailable, renamed: "streamCompleted")
-  public var final: StreamCompleted? { return streamCompleted }
 
   public var streamError: Error? {
-    if case .error(let error) = state, !(error is StreamCompleted)
+    if let error = error, !(error is StreamCompleted)
     { return error }
 
     return nil
   }
-
-  @available(*, unavailable, renamed: "streamError")
-  public var error: Error? { return streamError}
 }
-
-extension Event: CustomStringConvertible
-{
-  public var description: String {
-    switch state
-    {
-    case .value(let value): return String(describing: value)
-    case .error(let final as StreamCompleted): return "\(final)"
-    case .error(let error): return "Error: \(error)"
-    }
-  }
-}
-
-#if swift (>=4.1)
-extension Event: Equatable where Value: Equatable
-{
-  public static func ==(lhs: Event, rhs: Event) -> Bool
-  {
-    switch (lhs.state, rhs.state)
-    {
-    case (.value(let lhv), .value(let rhv)):
-      return lhv == rhv
-    case (.error(let lhe), .error(let rhe)):
-      return String(describing: lhe) == String(describing: rhe)
-    default:
-      return false
-    }
-  }
-}
-#endif
-
-#if swift (>=4.2)
-@usableFromInline
-enum State<Value>
-{
-  case value(Value)
-  case error(Error)
-}
-#else
-enum State<Value>
-{
-  case value(Value)
-  case error(Error)
-}
-#endif
