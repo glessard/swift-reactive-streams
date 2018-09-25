e2bb804b2d370731a0c42a8a5a3a75e9d5b8f5ab
Guillaume Lessard <dhtnstff@gmail.com>
2018-09-25 10:53:54 -0600

    simplify StreamEvent.next

--- Sources/ReactiveStreams/stream-limited.swift
+++ Sources/ReactiveStreams/stream-limited.swift
@@ -7,11 +7,14 @@
 //
 
 import Dispatch
+import CAtomics
+import Outcome
 
 open class LimitedStream<InputValue, OutputValue>: SubStream<InputValue, OutputValue>
 {
-  let limit: Int64
-  var count: Int64 = 0
+  public let limit: Int64
+  private var counter = AtomicInt64()
+  public var count: Int64 { return counter.load(.relaxed) }
 
   public convenience init(qos: DispatchQoS = DispatchQoS.current, count: Int64)
   {
@@ -26,8 +29,28 @@ open class LimitedStream<InputValue, OutputValue>: SubStream<InputValue, OutputV
   init(validated: ValidatedQueue, count: Int64)
   {
     assert(count >= 0)
+    counter.initialize(0)
     self.limit = count
     super.init(validated: validated)
+  }
+
+  open override func dispatch(_ event: Outcome<OutputValue>)
+  {
+#if DEBUG && (os(macOS) || os(iOS) || os(tvOS) || os(watchOS))
+    if #available(iOS 10, macOS 10.12, tvOS 10, watchOS 3, *)
+    {
+      dispatchPrecondition(condition: .onQueue(queue))
+    }
+#endif
+
+    guard count < limit else { return }
+
+    super.dispatch(event)
+    let c = 1+counter.fetch_add(1, .relaxed)
+    if c == limit && event.isValue
+    { // close the stream if it hasn't been closed already
+      super.dispatch(Event.streamCompleted)
+    }
   }
 
   @discardableResult
--- Sources/ReactiveStreams/stream-next.swift
+++ Sources/ReactiveStreams/stream-next.swift
@@ -10,41 +10,17 @@ import Dispatch
 
 extension EventStream
 {
-  private func next(_ stream: LimitedStream<Value, Value>) -> EventStream<Value>
-  {
-    let limit = stream.limit
-    self.subscribe(
-      subscriber: stream,
-      subscriptionHandler: stream.setSubscription,
-      notificationHandler: {
-        mapped, event in
-        mapped.queue.async {
-          switch mapped.count+1
-          {
-          case let c where c < limit:
-            mapped.count = c
-            mapped.dispatch(event)
-          case limit:
-            mapped.count = limit
-            mapped.dispatch(event)
-            if event.isValue { mapped.close() }
-          default:
-            break
-          }
-          return
-        }
-      }
-    )
-    return stream
-  }
-
   public func next(qos: DispatchQoS? = nil, count: Int) -> EventStream<Value>
   {
-    return next(LimitedStream<Value, Value>(qos: qos ?? self.qos, count: Int64(count)))
+    let stream = LimitedStream<Value, Value>(qos: qos ?? self.qos, count: Int64(count))
+    self.subscribe(substream: stream)
+    return stream
   }
 
   public func next(_ queue: DispatchQueue, count: Int) -> EventStream<Value>
   {
-    return next(LimitedStream<Value, Value>(queue, count: Int64(count)))
+    let stream = LimitedStream<Value, Value>(queue, count: Int64(count))
+    self.subscribe(substream: stream)
+    return stream
   }
 }
