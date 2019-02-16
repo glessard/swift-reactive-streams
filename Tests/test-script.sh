d1451d54a228553a57883c9720ab53f62dc09bc0
Guillaume Lessard <dhtnstff@gmail.com>
2019-02-16 16:03:03 -0700

    update scripts, CI configuration and manifests

--- Tests/test-script.sh
+++ Tests/test-script.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 set -e
 
-if [[ -n "${SWIFT_TOOLS_VERSION}" ]]
+COMPILER_MAJOR_VERSION=`echo ${COMPILER_VERSION} | awk -F . '{print $1}'`
+TEST_OPTIONS="-c release"
+
+swift --version
+swift test ${TEST_OPTIONS}
+
+if [[ "${COMPILER_MAJOR_VERSION}" = "4" ]]
 then
-  swift package tools-version --set "${SWIFT_TOOLS_VERSION}"
+  MINOR_VERSION=`echo ${COMPILER_VERSION} | awk -F . '{print $2}'`
+  if [[ "${MINOR_VERSION}" = "2" ]]
+  then
+    VERSIONS="4"
+  fi
+elif [[ "${COMPILER_MAJOR_VERSION}" = "5" ]]
+then
+  VERSIONS="4.2 4"
 fi
 
-swift test
+for LANGUAGE_VERSION in $VERSIONS
+do
+  echo "" # add a small visual separation
+  echo "Testing in compatibility mode for Swift ${LANGUAGE_VERSION}"
+  swift package reset
+  rm -f Package.resolved
+  swift test ${TEST_OPTIONS} -Xswiftc -swift-version -Xswiftc ${LANGUAGE_VERSION}
+done
