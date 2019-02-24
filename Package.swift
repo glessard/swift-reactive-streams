9a0b79ac4aa8674f6a411e1a692940dda3a834e5
Guillaume Lessard <dhtnstff@gmail.com>
2019-02-24 16:56:12 -0700

    fix submodule revisions

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     .library(name: "ReactiveStreams", targets: ["ReactiveStreams"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/glessard/swift-atomics.git", from: "4.4.2"),
-    .package(url: "https://github.com/glessard/outcome.git", from: "4.2.0"),
-    .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.0.1"),
-    .package(url: "https://github.com/glessard/deferred.git", from: "4.4.2"),
+    .package(url: "https://github.com/glessard/swift-atomics.git", from: "4.4.3"),
+    .package(url: "https://github.com/glessard/outcome.git", from: "4.2.1"),
+    .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.0.2"),
+    .package(url: "https://github.com/glessard/deferred.git", from: "4.4.3"),
   ],
   targets: [
     .target(name: "ReactiveStreams", dependencies: ["CAtomics", "Outcome", "CurrentQoS", "deferred"]),
