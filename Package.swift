efd9dc1939216678fbbf8505cbb94ec2e17ba9f4
Guillaume Lessard <dhtnstff@gmail.com>
2019-05-03 23:01:24 -0600

    update dependencies

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
     .library(name: "ReactiveStreams", targets: ["ReactiveStreams"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/glessard/swift-atomics.git", .revision("2575543d151c992d7b0e9dd0929ee6f5482cbfdf")),
+    .package(url: "https://github.com/glessard/swift-atomics.git", .revision("2ede65e5947d68440778d2d6bbceb1dfdafc6f9a")),
     .package(url: "https://github.com/glessard/outcome.git", from: "4.2.3"),
     .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.1.0"),
-    .package(url: "https://github.com/glessard/deferred.git", .revision("742348fb486e6748c79fbaba4128056dccb57733")),
+    .package(url: "https://github.com/glessard/deferred.git", .revision("778450eb125c40403799a909aa3f3f690f1805ac")),
   ],
   targets: [
     .target(name: "ReactiveStreams", dependencies: ["CAtomics", "Outcome", "CurrentQoS", "deferred"]),
