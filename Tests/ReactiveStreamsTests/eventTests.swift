f6228e9ceb5a73d8949e16beae22432c70567cc2
Guillaume Lessard <dhtnstff@gmail.com>
2019-05-23 15:08:25 -0600

    add event tests

--- Tests/ReactiveStreamsTests/eventTests.swift
+++ Tests/ReactiveStreamsTests/eventTests.swift
@@ -16,26 +16,77 @@ class eventTests: XCTestCase
   {
     let value = Event<Int>(value: .max)
     XCTAssertNotNil(value.value)
-    XCTAssertNil(value.streamError)
-    XCTAssertNil(value.streamCompleted)
     XCTAssertNil(value.error)
+    XCTAssertEqual(value.isValue, true)
+    XCTAssertEqual(value.isError, false)
+    XCTAssertEqual(value.completedNormally, false)
 
     let error = Event<Int>(error: TestError.value(.min))
     XCTAssertNil(error.value)
-    XCTAssertNotNil(error.streamError)
-    XCTAssertNil(error.streamCompleted)
     XCTAssertNotNil(error.error)
+    XCTAssertEqual(error.isValue, false)
+    XCTAssertEqual(error.isError, true)
+    XCTAssertEqual(error.completedNormally, false)
 
-    let final = Event<Int>.streamCompleted
-    XCTAssertNil(final.value)
-    XCTAssertNil(final.streamError)
-    XCTAssertNotNil(final.streamCompleted)
-    XCTAssertNotNil(final.error)
+    let finalA = Event<Int>.streamCompleted
+    let finalB = Event<Int>(error: StreamCompleted.normally)
+    XCTAssertNil(finalA.value)
+    XCTAssertNil(finalA.error)
+    XCTAssertEqual(finalA.completedNormally, true)
+    XCTAssertEqual(finalA, finalB)
 
     let tardy = Event<Int>(error: StreamCompleted.lateSubscription)
     XCTAssertNil(tardy.value)
-    XCTAssertNotNil(tardy.streamError)
-    XCTAssertNotNil(tardy.streamCompleted)
     XCTAssertNotNil(tardy.error)
+    XCTAssertEqual(tardy.completedNormally, false)
+  }
+
+  func testDescription()
+  {
+    let i1 = nzRandom()
+    let o1 = Event(value: i1)
+    let d1 = String(describing: o1)
+    XCTAssert(d1.contains(String(describing: i1)))
+
+    let e2 = TestError(nzRandom())
+    let o2 = Event<Unicode.Scalar>(error: e2)
+    let d2 = String(describing: o2)
+    XCTAssert(d2.contains(String(describing: e2)))
+
+    let o3 = Event<Error>.streamCompleted
+    let d3 = String(describing: o3)
+    XCTAssertEqual(d3, "Stream Completed")
+  }
+
+  func testEquals()
+  {
+    let i1 = nzRandom()
+    let i2 = nzRandom()
+    let i3 = i1*i2
+
+    let e3 = Event(value: i1*i2)
+    XCTAssert(e3 == Event(value: i3))
+    XCTAssert(e3 != Event(value: i2))
+
+    var e4 = e3
+    e4 = Event(error: TestError(i1))
+    XCTAssert(e3 != e4)
+    XCTAssert(e4 != Event(error: TestError(i2)))
+
+    var e5 = e4
+    e5 = Event.streamCompleted
+    XCTAssert(e5 != e3)
+    XCTAssert(e5 == Event.streamCompleted)
+  }
+
+  func testHashable()
+  {
+    let e1 = Event<Double>.streamCompleted
+    let e2 = Event(value: 5.1)
+    let e3 = Event<Double>(error: TestError())
+
+    let s: Set = [e1, e2, e3]
+
+    XCTAssertTrue(s.contains(e2))
   }
 }
