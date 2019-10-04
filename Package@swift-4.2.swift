a4fd2786f6e184477665bb9ea9b74199f2451903
Guillaume Lessard <dhtnstff@gmail.com>
2019-10-04 17:55:27 -0600

    update dependency to Deferred

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -11,7 +11,7 @@ let package = Package(
     .package(url: "https://github.com/glessard/swift-atomics.git", from: "5.0.0"),
     .package(url: "https://github.com/glessard/outcome.git", from: "4.3.0"),
     .package(url: "https://github.com/glessard/CurrentQoS.git", from: "1.1.0"),
-    .package(url: "https://github.com/glessard/deferred.git", from: "5.1.0"),
+    .package(url: "https://github.com/glessard/deferred.git", from: "5.1.4"),
   ],
   targets: [
     .target(name: "ReactiveStreams", dependencies: ["CAtomics", "Outcome", "CurrentQoS", "deferred"]),
